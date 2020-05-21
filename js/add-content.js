b1daee88eaeb54a5056d4e9e43722800933aba39
GitOutside <colleensexton70@gmail.com>
2020-05-20 20:38:44 -0700

    trying and trying

--- js/add-content.js
+++ js/add-content.js
@@ -3,16 +3,11 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Good Evening!'; 
+    greeting ='Good evening!';
+} else if (hourNow > 12) {
+    greeting ='Ggood afternoon!';
+} else if (hourNow > 0) {
+    greeting ='Good morning!';
+} else {
+    greeting ='Welcome';
 }
-else if (hourNow > 12) {
-    greeting = 'Good Afternoon!';
-}
-else if (hourNow > 0) {
-    greeting = 'Good morning!'; 
-}
-else {
-    greeting = 'Welcome!/;
-}
-
-document.write('<h3>' + greeting + '</h3>');
